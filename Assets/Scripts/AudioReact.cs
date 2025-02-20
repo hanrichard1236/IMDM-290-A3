0b5e2bba16c5de71496eabd45a23f18b2c19c19e
hanrichard1236 <132407919+hanrichard1236@users.noreply.github.com>
2025-02-20 13:39:35 -0500

    reactiuve

---
+++ Assets/Scripts/AudioReact.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class AudioReact : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float vertMax = 1.5f;
+    public float vertMin = 0.8f;
+    public float horMax = 1.2f;
+    public float horMin = 0.8f;
+    public float audioMult = 1f;
+    public float smoothSpeed = 5f;
+    private Vector3 initialScale;
+    private Vector3 initialPos;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        initialScale = transform.localScale;
+        initialPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.localPosition;
+        // Get audio spectrum data
+        float[] spectrum = new float[256];
+        audioSource.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+
+        // Calculate intensity based on audio
+        float intensity = Mathf.Clamp(spectrum[5] * audioMult, 0f, 1f);
+
+        // Interpolate vertical and horizontal scaling
+        float targetY = Mathf.Lerp(vertMin * initialScale[1], vertMax * initialScale[1], intensity);
+        float targetX = Mathf.Lerp(horMin * initialScale[0], horMax * initialScale[0], 1f - intensity);
+        float targetZ = targetX;
+
+        // Smoothly scale the object
+        transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(targetX, targetY, targetZ), Time.deltaTime * smoothSpeed);
+
+        // Anchor the base by adjusting position
+        float heightOffset = (transform.localScale.y - initialScale.y) / 2f;
+        transform.localPosition = new Vector3(position[0], initialPos[1], position[2]) + new Vector3(0, heightOffset, 0);
+    }
+}
---
+++ Assets/Scripts/Sun Reaction.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SunReaction : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float scaleMax = 1.5f;
+    public float scaleMin = 0.8f;
+    public float audioMult = 1f;
+    public float smoothSpeed = 5f;
+    private Vector3 initialScale;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        initialScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float[] spectrum = new float[256];
+        audioSource.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+
+        // Calculate intensity based on audio
+        float intensity = Mathf.Clamp(spectrum[5] * audioMult, 0f, 1f);
+
+        float target = Mathf.Lerp(scaleMin * initialScale[0], scaleMax * initialScale[0], intensity);
+
+        transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(target, target, target), Time.deltaTime * smoothSpeed);
+    }
+}
