cfa31775ae4affc70914631e2d455015c81d2a3f
hanrichard1236 <132407919+hanrichard1236@users.noreply.github.com>
2025-02-20 13:46:06 -0500

    small changes

--- Assets/Scripts/Sun Reaction.cs
+++ Assets/Scripts/Sun Reaction.cs
@@ -8,12 +8,15 @@ public class SunReaction : MonoBehaviour
     public float audioMult = 1f;
     public float smoothSpeed = 5f;
     private Vector3 initialScale;
-    
+    private Renderer m_Renderer;
+    public float minBrightness = 1f;
+    public float maxBrightness = 3f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         initialScale = transform.localScale;
+        m_Renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -28,5 +31,7 @@ public class SunReaction : MonoBehaviour
         float target = Mathf.Lerp(scaleMin * initialScale[0], scaleMax * initialScale[0], intensity);
 
         transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(target, target, target), Time.deltaTime * smoothSpeed);
+
+        //m_Renderer.material.color[2] = Mathf.Lerp(minBrightness, maxBrightness, intensity);
     }
 }
